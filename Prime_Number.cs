11afa49fe097729f03d5b4114cc2a5e2fb932fde
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-08-13 22:43:48 +0530

    Add files via upload

---
+++ Prime_Number.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximum
+{
+    class Prime_Number
+    {
+        public static void Main(String[] args)
+        {
+            int flag = 0, n, i,start,end;
+            Console.WriteLine("Enter the number : ");
+            n = int.Parse(Console.ReadLine());
+            //prime number condition
+            for(i=2; i<=n/2; i++)
+            {
+                if(n%i==0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if(n==1)
+            {
+                Console.WriteLine("1 is neither prime nor composite");
+            }
+            else
+            {
+                if (flag == 0)
+                {
+                    Console.WriteLine(n + " is a prime number");
+                }
+                else
+                {
+                    Console.WriteLine(n + " is not a prime number");
+                }
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prime_number_between_ranges.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maximum
+{
+    class Prime_number_between_ranges
+    {
+        public static void Main(String[] args)
+        {
+            int start, end, i;
+            int f = 0;
+            Console.WriteLine("Enter the starting number : ");
+            start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the ending number : ");
+            end = int.Parse(Console.ReadLine());
+
+            while(start > end)
+            {
+                
+                for (i=2; i<=start/2; i++)
+                {
+                    if(start % i == 0)
+                    {
+                        f = 1;
+                        break;
+                    }
+                }
+                // if f==0 then it is prime number, so we are incrementing the start value using while loop
+                if(f==0)
+                {
+                    Console.Write("" + start);
+                    ++start;
+                }
+               
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
