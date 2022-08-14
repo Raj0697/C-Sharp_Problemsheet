a11f4e95f301cb65316c14403fa09c7469d92900
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-08-14 21:58:34 +0530

    Add files via upload

---
+++ Constructors_with_Modifiers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+    class vehicle
+    {
+        String name;
+        int engine_no;
+        String petrol_type;
+
+        public vehicle()
+        {
+            Console.WriteLine("=====vehicle information=========");
+        }
+        
+        protected vehicle(String name,int eno)
+        {
+            this.name = name;
+            this.engine_no = eno;
+        }
+
+        private vehicle(int eng_no)
+        {
+            this.engine_no = eng_no;
+        }
+
+        protected vehicle(String petrol_type)
+        {
+            this.petrol_type = petrol_type;
+        }
+
+        
+    }
+    class car:vehicle
+    {
+        int t = 0;
+        public void dis()
+        {
+            Console.WriteLine(t);
+        }
+    }
+    class Constructors_with_Modifiers
+    {
+        public static void Main(String[] args)
+        {
+            car c = new car();
+            c.dis();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Inheritance_modifiers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers
+{
+    class A
+    {
+        protected int x=10;
+        public void print()
+        {
+            Console.WriteLine(x);
+        }
+    }
+    class B:A
+    {
+        protected int y=90;
+        public void show()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+    class C:B
+    {
+        private int z = 88;
+        public void pr()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+    class Inheritance_modifiers
+    {
+        public static void Main(String[] args)
+        {
+            C obj = new C();
+            obj.pr();
+            Console.ReadKey();
+        }
+    }
+}
